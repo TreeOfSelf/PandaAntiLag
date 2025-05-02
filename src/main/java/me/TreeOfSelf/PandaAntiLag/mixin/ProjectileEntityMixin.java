2057ebb155159ff7063627f7133ce26668ef9701
Sebastian <sebastian97@gmail.com>
2025-05-02 00:18:47 -0700

    Update ProjectileEntityMixin.java

--- src/main/java/me/TreeOfSelf/PandaAntiLag/mixin/ProjectileEntityMixin.java
+++ src/main/java/me/TreeOfSelf/PandaAntiLag/mixin/ProjectileEntityMixin.java
@@ -2,18 +2,21 @@ package me.TreeOfSelf.PandaAntiLag.mixin;
 
 import me.TreeOfSelf.PandaAntiLag.AntiLagSettings;
 import me.TreeOfSelf.PandaAntiLag.LagPos;
+import me.TreeOfSelf.PandaAntiLag.PandaAntiLag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.TypeFilter;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(ProjectileEntity.class)
 public abstract class ProjectileEntityMixin extends Entity {
@@ -22,29 +25,37 @@ public abstract class ProjectileEntityMixin extends Entity {
         super(type, world);
     }
 
-    @Inject(method = "<init>", at = @At(value = "TAIL"))
-    public void init(EntityType entityType, World world, CallbackInfo ci){
-        ServerWorld serverWorld = (ServerWorld) world;
-        ProjectileEntity projectileEntity = (ProjectileEntity)(Object)this;
-        LagPos lagPos = new LagPos(projectileEntity.getChunkPos());
-        List<? extends ProjectileEntity> nearbyEntities = serverWorld.getEntitiesByType(
+    private static <T extends ProjectileEntity> void handleProjectileSpawn(T projectile, ServerWorld world) {
+        LagPos lagPos = new LagPos(projectile.getChunkPos());
+        List<? extends ProjectileEntity> nearbyEntities = world.getEntitiesByType(
                 TypeFilter.instanceOf(ProjectileEntity.class),
                 entity -> {
                     LagPos thisLagPos = new LagPos(entity.getChunkPos());
-                    return true;
-                    /*return Math.abs(thisLagPos.x - lagPos.x) < AntiLagSettings.regionBuffer &&
-                            Math.abs(thisLagPos.z - lagPos.z) < AntiLagSettings.regionBuffer &&
-                            entity.getClass() == projectileEntity.getClass();*/
+                    return Math.abs(thisLagPos.x - lagPos.x) <= AntiLagSettings.regionBuffer &&
+                            Math.abs(thisLagPos.z - lagPos.z) <= AntiLagSettings.regionBuffer
+                            && entity.getType() == projectile.getType();
                 }
         );
 
         int nearby = nearbyEntities.size();
+
+        PandaAntiLag.LOGGER.info("nearby {}", nearby);
+
         if (nearby > AntiLagSettings.projectileMax) {
             int over = (nearby - AntiLagSettings.projectileMax);
-            for(int index = 0; index <= over; index++){
+            for (int index = 0; index <= over; index++) {
                 nearbyEntities.getFirst().remove(RemovalReason.KILLED);
             }
         }
     }
-}
 
+    @Inject(method = "spawn(Lnet/minecraft/entity/projectile/ProjectileEntity;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/projectile/ProjectileEntity;", at = @At(value = "TAIL"))
+    private static <T extends ProjectileEntity> void spawn(T projectile, ServerWorld world, ItemStack projectileStack, CallbackInfoReturnable<T> cir) {
+        handleProjectileSpawn(projectile, world);
+    }
+
+    @Inject(method = "spawn(Lnet/minecraft/entity/projectile/ProjectileEntity;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/item/ItemStack;Ljava/util/function/Consumer;)Lnet/minecraft/entity/projectile/ProjectileEntity;", at = @At(value = "TAIL"))
+    private static <T extends ProjectileEntity> void spawn(T projectile, ServerWorld world, ItemStack projectileStack, Consumer<T> beforeSpawn, CallbackInfoReturnable<T> cir) {
+        handleProjectileSpawn(projectile, world);
+    }
+}
